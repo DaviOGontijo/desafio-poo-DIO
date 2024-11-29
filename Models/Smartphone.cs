99c575a8f68e5494e365409ce3ca035cfad5e6b4
daviogontijo <davi0400@gmail.com>
2024-11-29 10:30:40 -0300

    Realizado as alterações solicitadas.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,23 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+
+        //Necessario caso queira passar o parâmetro direto no model (Nokia ou Iphone)
+        //public Smartphone()
+        //{
+
+        //}
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando o Nokia:");
+        Smartphone nokia = new Nokia("123456", "Modelo X", "111111111", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTestando o iPhone:");
+        Smartphone iphone = new Iphone("654321", "Modelo Y", "222222222", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
